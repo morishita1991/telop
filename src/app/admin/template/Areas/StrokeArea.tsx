c26864d637e324e2a090bbf798b16bcfed06a596
morishita1991 <vw2gmldqbelief@gmail.com>
2023-10-04 22:06:07 +0900

    ストローク生成

--- src/app/admin/template/Areas/StrokeArea.tsx
+++ src/app/admin/template/Areas/StrokeArea.tsx
@@ -1,23 +1,86 @@
 import React from 'react';
 
-const StrokeArea = () => {
+export default function StrokeArea() {
+  const newStrokeNum = 1;
+  const newStrokeId = 'addStroke_' + newStrokeNum;
   return (
     <>
       <div className="m-2 ms-5 align-top">
         <div style={{ maxHeight: '50px' }}>
           <div className="d-flex flex-row">
-            <input className="btn btn-default align-top" type="checkbox" id="strokeCheckBox" data-bs-toggle="collapse" data-bs-target="#strokeAria" aria-expanded="false" aria-controls="collapseExample" style={{ transform: 'scale(1.5)' }}></input>
+            <input
+              className="btn btn-default align-top"
+              type="checkbox"
+              id="strokeCheckBox"
+              data-bs-toggle="collapse"
+              data-bs-target="#strokeAria"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+              style={{ transform: 'scale(1.5)' }}
+            >
+            </input>
             <label className="form-check-label ms-3 mt-1 h4" htmlFor='strokeCheckBox'>ストローク</label>
           </div>
         </div>
       </div>
       <div className="border ms-3 mb-5 collapse" id="strokeAria" style={{ maxWidth: '1080px' }}>
+        <div className="row" id={"stroke_" + newStrokeNum}>
+          {/* <div className="col-sm-11" style={{ maxWidth: '980px' }}>
+            <div className="row border m-3 me-3 h5">
+              <div className="row p-3 ms-3">
+                <div className="col-sm-4 pt-4 mt-2 mb-3 me-4" style={{ maxWidth: '150px' }}>
+                  <div className="form-check">
+                    <input className="form-check-input" type="radio" name="strokeEdge" id={"stroke-edge-inner-" + newStrokeNum}></input>
+                    <label className="form-check-label" htmlFor={"stroke-edge-inner-" + newStrokeNum}>
+                      内側
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="radio" name="strokeEdge" checked id={"stroke-edge-outer-" + newStrokeNum}></input>
+                    <label className="form-check-label" htmlFor={"stroke-edge-outer-" + newStrokeNum}>
+                      外側
+                    </label>
+                  </div>
+                </div>
+                <div className="col-sm-4 me-5" style={{ maxWidth: '200px' }}>
+                  <div className="row">
+                    <div className="col-12 mt-4">
+                      <span className="mb-2" style={{ display: 'inline-block' }}>太さ</span>
+                      <input type="text" className="form-control ms-2" id={"input-stroke-weight-" + newStrokeNum} value="0" style={{ maxWidth: '50px', display: 'inline-block' }}></input>
+                      <input type="range" className="form-range" min="0" max="20" step="1" id={"strokeWidthRange" + newStrokeNum} value="0" style={{ maxWidth: '200px' }}></input>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-4" style={{ maxWidth: '200px' }}>
+                  <div className="col-12 mt-3">
+                    <span style={{ display: 'inline-block' }}>色</span>
+                    <input type="text" className="form-control ms-2" id={"input-color-storke-" + newStrokeNum} value="#000000" style={{ maxWidth: '100px', display: 'inline-block' }}></input>
+                    <input type="color" className="form-control form-control-color mt-2" id={"color-stroke-" + newStrokeNum} name="color" style={{ fontSize: '20px' }} value="#000000"></input>
+                  </div>
+                </div>
+                <div className="col-sm-4 ms-3" style={{ maxWidth: '200px' }}>
+                  <div className="row">
+                    <div className="col-12 mt-4">
+                      <span style="display: inline-block">不透明度</span>
+                      <input type="text" className="form-control ms-2" id={"input-opacity-stroke-" + newStrokeNum} value="1" style={{ maxWidth: '50px', display: 'inline-block' }}></input>
+                      <input type="range" className="form-range" min="0" max="1" step="0.1" id={"strokeOpacityRange" + newStrokeNum} value="1" style={{ maxWidth: '200px' }}></input>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-1 m-2">
+            <div className="col-12 mt-4 pt-3 h6" id={"additionalStrokeDiv_" + newStrokeNum}>
+              <p className="mt-2 mb-2">追加</p>
+              <button type="button" className="btn btn-secondary rounded-circle p-0" id={"addStroke_" + newStrokeNum} style={{ width: '2rem', height: '2rem' }}>＋</button>
+            </div>
+          </div> */}
+        </div>
       </div>
     </>
   );
 }
-
-export default StrokeArea;
 
 
 // export class Stroke {
@@ -117,74 +180,5 @@ export default StrokeArea;
 //   static removeStroke(strokeNum) {
 //     const strokeDiv = document.getElementById('stroke_' + strokeNum);
 //     strokeDiv.remove();
-//   }
-
-//   static addStroke(strokeId = null) {
-//     if (!strokeId) {
-//       strokeId = 'addStroke_0';
-//     }
-//     const strokeArr = strokeId.split('_');
-//     if (strokeArr.length !== 2) {
-//       return null;
-//     }
-//     const newStrokeNum = String(Number(strokeArr[1]) + 1);
-//     const newStrokeId = 'addStroke_' + newStrokeNum;
-
-//     const html = `<div class="row" id="stroke_${newStrokeNum}">
-//       <div class="col-sm-11" style="max-width:980px">
-//         <div class="row border m-3 me-3 h5">
-//           <div class="row p-3 ms-3">
-//             <div class="col-sm-4 pt-4 mt-2 mb-3 me-4" style="max-width:150px">
-//               <div class="form-check">
-//                 <input class="form-check-input" type="radio" name="strokeEdge" id="stroke-edge-inner-${newStrokeNum}">
-//                 <label class="form-check-label" for="stroke-edge-inner-${newStrokeNum}">
-//                   内側
-//                 </label>
-//               </div>
-//               <div class="form-check">
-//                 <input class="form-check-input" type="radio" name="strokeEdge" checked id="stroke-edge-outer-${newStrokeNum}">
-//                 <label class="form-check-label" for="stroke-edge-outer-${newStrokeNum}">
-//                   外側
-//                 </label>
-//               </div>
-//             </div>
-//             <div class="col-sm-4 me-5" style="max-width:200px">
-//               <div class="row">
-//                 <div class="col-12 mt-4">
-//                   <span class="mb-2" style="display: inline-block">太さ</span>
-//                   <input type="text" class="form-control ms-2" id="input-stroke-weight-${newStrokeNum}" value="0" style="max-width: 50px; display: inline-block">
-//                   <input type="range" class="form-range" min="0" max="20" step="1" id="strokeWidthRange${newStrokeNum}" value="0" style="max-width:200px">
-//                 </div>
-//               </div>
-//             </div>
-//             <div class="col-sm-4" style="max-width:200px">
-//               <div class="col-12 mt-3">
-//                 <span style="display: inline-block">色</span>
-//                 <input type="text" class="form-control ms-2" id="input-color-storke-${newStrokeNum}" value="#000000" style="max-width:100px; display: inline-block">
-//                 <input type="color" class="form-control form-control-color mt-2" id="color-stroke-${newStrokeNum}" name="color" style="font-size:20px;" value="#000000">
-//               </div>
-//             </div>
-//             <div class="col-sm-4 ms-3" style="max-width:200px">
-//               <div class="row">
-//                 <div class="col-12 mt-4">
-//                   <span style="display: inline-block">不透明度</span>
-//                   <input type="text" class="form-control ms-2" id="input-opacity-stroke-${newStrokeNum}" value="1" style="max-width:50px; display: inline-block">
-//                   <input type="range" class="form-range" min="0" max="1" step="0.1" id="strokeOpacityRange${newStrokeNum}" value="1" style="max-width:200px">
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div class="col-sm-1 m-2">
-//         <div class="col-12 mt-4 pt-3 h6" id="additionalStrokeDiv_${newStrokeNum}">
-//           <p class="mt-2 mb-2">追加</p>
-//           <button type="button" class="btn btn-secondary rounded-circle p-0" id="${newStrokeId}" style="width:2rem;height:2rem;">＋</button>
-//         </div>
-//       </div>
-//     </div>`;
-
-//     document.getElementById('strokeAria').insertAdjacentHTML('beforeend', html);
-//     return newStrokeId;
 //   }
 // }
